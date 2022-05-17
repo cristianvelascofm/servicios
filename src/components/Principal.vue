ad1611681d67a3e0b9dbca33fddd4e993eac109d
cfvelasco <cfvelasco@unicauca.edu.co>
2022-05-17 16:49:42 -0500

    Logo

--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -39,50 +39,83 @@
             required
           ></b-form-input>
         </b-form-group>
+        <b-form-group
+          label="Primer Apellido"
+          label-for="first-last-name-input"
+          invalid-feedback="Primer apellido es requerido"
+          :state="firstLastNameState"
+        >
+          <b-form-input
+            id="name-input"
+            v-model="fistLastName"
+            :state="firstLastNameState"
+            required
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group
+          label="Segundo Apellido"
+          label-for="second-last-name-input"
+          invalid-feedback="Segundo apellido es requerido"
+          :state="secondLastNameState"
+        >
+          <b-form-input
+            id="second-name-input"
+            v-model="secondLastName"
+            :state="secondLastNameState"
+            required
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group>
+          <b-input-group prepend="@" class="mb-2 mr-sm-2 mb-sm-0">
+            <b-form-input
+              id="username-input"
+              placeholder="Username"
+            ></b-form-input>
+          </b-input-group>
+        </b-form-group>
       </form>
     </b-modal>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        name: '',
-        nameState: null,
-        lastName: '',
-        lastNameState: null,
-        
-      }
+export default {
+  data() {
+    return {
+      name: "",
+      nameState: null,
+      lastName: "",
+      lastNameState: null,
+    };
+  },
+  methods: {
+    checkFormValidity() {
+      const valid = this.$refs.form.checkValidity();
+      this.nameState = valid;
+      return valid;
     },
-    methods: {
-      checkFormValidity() {
-        const valid = this.$refs.form.checkValidity()
-        this.nameState = valid
-        return valid
-      },
-      resetModal() {
-        this.name = ''
-        this.nameState = null
-      },
-      handleOk(bvModalEvent) {
-        // Prevent modal from closing
-        bvModalEvent.preventDefault()
-        // Trigger submit handler
-        this.handleSubmit()
-      },
-      handleSubmit() {
-        // Exit when the form isn't valid
-        if (!this.checkFormValidity()) {
-          return
-        }
-        // Push the name to submitted names
-        this.submittedNames.push(this.name)
-        // Hide the modal manually
-        this.$nextTick(() => {
-          this.$bvModal.hide('modal-user')
-        })
+    resetModal() {
+      this.name = "";
+      this.nameState = null;
+    },
+    handleOk(bvModalEvent) {
+      // Prevent modal from closing
+      bvModalEvent.preventDefault();
+      // Trigger submit handler
+      this.handleSubmit();
+    },
+    handleSubmit() {
+      // Exit when the form isn't valid
+      if (!this.checkFormValidity()) {
+        return;
       }
-    }
-  }
+      // Push the name to submitted names
+      this.submittedNames.push(this.name);
+      // Hide the modal manually
+      this.$nextTick(() => {
+        this.$bvModal.hide("modal-user");
+      });
+    },
+  },
+};
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img src="../assets/logo.gif" fluid alt="Fluid image" width="300px">
     <Principal msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
