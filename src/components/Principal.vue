4180fd27f778393e7f9f0b19e72504ed89039a21
Cristian Velasco <cfvelasco@unicauca.edu.co>
2022-05-19 00:10:30 -0500

    User form

--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -5,137 +5,256 @@
       <b-modal
         id="modal-user"
         ref="modal"
-        title="Usuario"
+        title="Usuarios"
         @show="resetModal"
         @hidden="resetModal"
         @ok="handleOk"
+        header-bg-variant="light"
+        header-text-variant="dark"
         hide-header-close
         no-close-on-esc
         no-close-on-backdrop
+        size="xl"
       >
-        <b-container>
-          <b-row id="lolo" xs-12>
-            <b-col id="lala" color="primary">
-              <b-form-group
-                label="Selection mode:"
-                label-for="table-select-mode-select"
-                label-cols-md="4"
-              >
-                <b-form-select
-                  id="table-select-mode-select"
-                  v-model="selectMode"
-                  :options="modes"
-                  class="mb-3"
-                ></b-form-select>
-              </b-form-group>
+        <b-container class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+          <b-row id="lolo" class="m-0 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <!-- Formulario Usuario -->
+            <b-col
+              id="lala"
+              color="primary"
+              class="col-sm-6 col-md-6 col-lg-6 col-xl-6"
+            >
+              <b-row>
+                <b-col>
+                  <form ref="form" @submit.stop.prevent="handleSubmit">
+                    <b-form-group
+                      label="Primer Nombre"
+                      label-for="name-input"
+                      invalid-feedback="Primer nombre es requerido"
+                      :state="nameState"
+                    >
+                      <b-form-input
+                        id="name-input"
+                        v-model="name"
+                        :state="nameState"
+                        required
+                      ></b-form-input>
+                    </b-form-group>
+                    <b-form-group
+                      label="Segundo Nombre"
+                      label-for="second-name-input"
+                      invalid-feedback="Segundo nombre es requerido"
+                      :state="lastNameState"
+                    >
+                      <b-form-input
+                        id="second-name-input"
+                        v-model="secondName"
+                        :state="secondNameState"
+                        required
+                      ></b-form-input>
+                    </b-form-group>
+                    <b-form-group
+                      label="Primer Apellido"
+                      label-for="first-last-name-input"
+                      invalid-feedback="Primer apellido es requerido"
+                      :state="firstLastNameState"
+                    >
+                      <b-form-input
+                        id="name-input"
+                        v-model="fistLastName"
+                        :state="firstLastNameState"
+                        required
+                      ></b-form-input>
+                    </b-form-group>
+                    <b-form-group
+                      label="Segundo Apellido"
+                      label-for="second-last-name-input"
+                      invalid-feedback="Segundo apellido es requerido"
+                      :state="secondLastNameState"
+                    >
+                      <b-form-input
+                        id="second-name-input"
+                        v-model="secondLastName"
+                        :state="secondLastNameState"
+                        required
+                      ></b-form-input>
+                    </b-form-group>
 
-              <b-table
-                :items="items"
-                :fields="fields"
-                :select-mode="selectMode"
-                responsive="sm"
-                ref="selectableTable"
-                selectable
-                @row-selected="onRowSelected"
-              >
-                <!-- Example scoped slot for select state illustrative purposes -->
-                <template #cell(selected)="{ rowSelected }">
-                  <template v-if="rowSelected">
-                    <span aria-hidden="true">&check;</span>
-                    <span class="sr-only">Selected</span>
-                  </template>
-                  <template v-else>
-                    <span aria-hidden="true">&nbsp;</span>
-                    <span class="sr-only">Not selected</span>
-                  </template>
-                </template>
-              </b-table>
-              <p>
-                <b-button size="sm" @click="selectAllRows">Select all</b-button>
-                <b-button size="sm" @click="clearSelected"
-                  >Clear selected</b-button
-                >
-                <b-button size="sm" @click="selectThirdRow"
-                  >Select 3rd row</b-button
-                >
-                <b-button size="sm" @click="unselectThirdRow"
-                  >Unselect 3rd row</b-button
-                >
-              </p>
-              <p>
-                Selected Rows:<br />
-                {{ selected }}
-              </p>
+                    <b-form-group
+                      label="Usuario"
+                      label-for="user-input"
+                      invalid-feedback="Usuario es requerido"
+                      :state="userState"
+                    >
+                      <b-form-input
+                        id="user-input"
+                        v-model="user"
+                        :state="userState"
+                        required
+                      ></b-form-input>
+                    </b-form-group>
+                    <b-form-group
+                      label="Contraseña"
+                      label-for="password-input"
+                      invalid-feedback="Contraseña es requerido"
+                      :state="passwordState"
+                    >
+                      <b-form-input
+                        id="password-input"
+                        v-model="password"
+                        :state="passwordState"
+                        required
+                      ></b-form-input>
+                    </b-form-group>
+                    <b-form-group
+                      label="E-mail"
+                      label-for="mail-input"
+                      invalid-feedback="E-mail es requerido"
+                      :state="mailState"
+                    >
+                      <b-form-input
+                        id="mail-input"
+                        v-model="mail"
+                        :state="mailState"
+                        required
+                      ></b-form-input>
+                    </b-form-group>
+                    <b-row>
+                      <b-col class="col-xs-6">
+                        <b-form-group
+                          label="Empresa"
+                          label-for="build-input"
+                          invalid-feedback="Empresa es requerida"
+                          :state="buildState"
+                        >
+                          <b-form-select
+                            id="build-input"
+                            v-model="build"
+                            :state="buildState"
+                            :options="builds"
+                            required
+                          ></b-form-select>
+                        </b-form-group>
+                      </b-col>
+                      <b-col>
+                        <b-form-group
+                          label="Nit Empresa"
+                          label-for="nit-build-input"
+                          invalid-feedback="Empresa es requerida"
+                          :state="nitBuildState"
+                        >
+                          <b-form-input
+                            id="-input"
+                            v-model="nitBuild"
+                            :state="nitBuildState"
+                            required
+                          ></b-form-input>
+                        </b-form-group>
+                      </b-col>
+                    </b-row>
+                      <b-row >
+                        <b-col class="col-sm-6">
+                          <b-form-group
+                            label="Rol"
+                            label-for="rol-input"
+                            invalid-feedback="Rol es requerido"
+                            :state="rolState"
+                          >
+                            <b-form-select
+                              id="rol-input"
+                              v-model="rol"
+                              :state="rolState"
+                              :options="rols"
+                              required
+                              size="lg"
+                            ></b-form-select>
+                          </b-form-group>
+                        </b-col>
+                        <b-col class="col-sm-6" >
+                          <b-form-group
+                            label="Estado"
+                            label-for="state-input"
+                            invalid-feedback="Estado es requerido"
+                            :state="stateState"
+                          >
+                            <b-form-select
+                              id="state-input"
+                              v-model="state"
+                              :state="stateState"
+                              :options="states"
+                              required
+                              size="sm"
+                            ></b-form-select>
+                          </b-form-group>
+                        </b-col>
+                      
+                    
+                    </b-row>
+                  </form>
+                </b-col>
+              </b-row>
             </b-col>
-            <b-col>
-              <form ref="form" @submit.stop.prevent="handleSubmit">
+
+            <!-- Tabla Usuarios -->
+            <b-col
+              id="lele"
+              color="primary"
+              class="col-sm-6 col-md-6 col-lg-6 col-xl-6"
+            >
+              <b-row>
                 <b-form-group
-                  label="Primer Nombre"
-                  label-for="name-input"
-                  invalid-feedback="Primer nombre es requerido"
-                  :state="nameState"
+                  label="Selection mode:"
+                  label-for="table-select-mode-select"
+                  label-cols-md="4"
                 >
-                  <b-form-input
-                    id="name-input"
-                    v-model="name"
-                    :state="nameState"
-                    required
-                  ></b-form-input>
+                  <b-form-select
+                    id="table-select-mode-select"
+                    v-model="selectMode"
+                    :options="modes"
+                    class="mb-3"
+                  ></b-form-select>
                 </b-form-group>
-                <b-form-group
-                  label="Segundo Nombre"
-                  label-for="second-name-input"
-                  invalid-feedback="Segundo nombre es requerido"
-                  :state="lastNameState"
+
+                <b-table
+                  :items="items"
+                  :fields="fields"
+                  :select-mode="selectMode"
+                  responsive="sm"
+                  ref="selectableTable"
+                  selectable
+                  @row-selected="onRowSelected"
                 >
-                  <b-form-input
-                    id="second-name-input"
-                    v-model="secondName"
-                    :state="secondNameState"
-                    required
-                  ></b-form-input>
-                </b-form-group>
-                <b-form-group
-                  label="Primer Apellido"
-                  label-for="first-last-name-input"
-                  invalid-feedback="Primer apellido es requerido"
-                  :state="firstLastNameState"
-                >
-                  <b-form-input
-                    id="name-input"
-                    v-model="fistLastName"
-                    :state="firstLastNameState"
-                    required
-                  ></b-form-input>
-                </b-form-group>
-                <b-form-group
-                  label="Segundo Apellido"
-                  label-for="second-last-name-input"
-                  invalid-feedback="Segundo apellido es requerido"
-                  :state="secondLastNameState"
-                >
-                  <b-form-input
-                    id="second-name-input"
-                    v-model="secondLastName"
-                    :state="secondLastNameState"
-                    required
-                  ></b-form-input>
-                </b-form-group>
-                <b-form-group
-                  label="Usuario"
-                  label-for="second-last-name-input"
-                  invalid-feedback="Segundo apellido es requerido"
-                  :state="secondLastNameState"
-                  class=""
-                >
-                  <b-input-group prepend="@" class="mb-2 mr-sm-2 mb-sm-0">
-                    <b-form-input
-                      id="username-input"
-                      placeholder="Username"
-                    ></b-form-input>
-                  </b-input-group>
-                </b-form-group>
-              </form>
+                  <!-- Example scoped slot for select state illustrative purposes -->
+                  <template #cell(selected)="{ rowSelected }">
+                    <template v-if="rowSelected">
+                      <span aria-hidden="true">&check;</span>
+                      <span class="sr-only">Selected</span>
+                    </template>
+                    <template v-else>
+                      <span aria-hidden="true">&nbsp;</span>
+                      <span class="sr-only">Not selected</span>
+                    </template>
+                  </template>
+                </b-table>
+                <p>
+                  <b-button size="sm" @click="selectAllRows"
+                    >Select all</b-button
+                  >
+                  <b-button size="sm" @click="clearSelected"
+                    >Clear selected</b-button
+                  >
+                  <b-button size="sm" @click="selectThirdRow"
+                    >Select 3rd row</b-button
+                  >
+                  <b-button size="sm" @click="unselectThirdRow"
+                    >Unselect 3rd row</b-button
+                  >
+                </p>
+                <p>
+                  Selected Rows:<br />
+                  {{ selected }}
+                </p>
+              </b-row>
             </b-col>
           </b-row>
         </b-container>
@@ -145,6 +264,7 @@
 </template>
 
 <script>
+import { ClientesService } from '../services/Client';
 export default {
   data() {
     return {
@@ -167,6 +287,30 @@ export default {
       ],
       selectMode: "multi",
       selected: [],
+      build: null,
+      builds: [
+        { value: null, text: "Please select an option" },
+        { value: "a", text: "This is First option" },
+        { value: "b", text: "Selected Option" },
+        { value: { C: "3PO" }, text: "This is an option with object value" },
+        { value: "d", text: "This one is disabled", disabled: true },
+      ],
+      rol: null,
+      rols: [
+        { value: null, text: "Please select an option" },
+        { value: "a", text: "This is First option" },
+        { value: "b", text: "Selected Option" },
+        { value: { C: "3PO" }, text: "This is an option with object value" },
+        { value: "d", text: "This one is disabled", disabled: true },
+      ],
+      state: null,
+      states: [
+        { value: null, text: "Please select an option" },
+        { value: "a", text: "This is First option" },
+        { value: "b", text: "Selected Option" },
+        { value: { C: "3PO" }, text: "This is an option with object value" },
+        { value: "d", text: "This one is disabled", disabled: true },
+      ],
     };
   },
   methods: {
@@ -219,7 +363,13 @@ export default {
 </script>
 
 <style>
-#lolo{
+/* #lala {
+  background-color: blue;
+}
+#lele{
+  background-color: brown;
+} */
+#aaa{
   background-color: aqua;
 }
 </style>
---
+++ src/services/Client.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+var path = ""
+var data = {}
+
+export const ClientService ={
+    async getClients(){
+        
+    },
+    async addClient(){
+        
+    },
+    async updateClient(){
+
+    },
+    async deleteClient(){
+        axios.post(path, data).then((response) => {
+
+        })
+    },
+    
+    
+}
